2084cba8dcd08f1345cf57a9894455409702681c
jcapron <elniafron62@gmail.com>
2013-02-21 15:32:38 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymoviesearch::Application.config.secret_token = '79916d8d3af4f8c4ec17d6fb7c566a4c492e5b861fe4d7026d00b36dede0bff022f2b9ba82e55c0074eacf31ae9d0b24859e01acbb731738e3f7f792ec6b8838'
