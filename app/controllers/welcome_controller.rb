b72bc59f5451228a758345a806cde0166968f4ae
jcapron <elniafron62@gmail.com>
2013-02-22 13:45:08 -0500

    check if the movie exists

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,10 @@ def index
 	if params[:search]
 		@video_res = yt_client.videos_by(:query => params[:search] + " trailer", :format => 5)
 			
-		@bf_movie_poster = bf_client.movies.search_by_name(params[:search])[0].posters.original
-
+		@bf_movies = bf_client.movies.search_by_name(params[:search])
+		if @bf_movies[0] 
+			@bf_movie_poster = @bf_movies[0].posters.original
+		end
 	end
 end
 
